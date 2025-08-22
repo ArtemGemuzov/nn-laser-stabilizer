d25b01324416e994abbf478eb9a531b01dd1d76a
ArtemGemuzov <artemgemuzov.en@gmail.com>
2025-08-22 18:27:11 +0700

    Добавил init файлы

--- src/nn_laser_stabilizer/data/utils.py
+++ src/nn_laser_stabilizer/data/utils.py
@@ -3,13 +3,12 @@ from torchrl.envs import TransformedEnv
 from torchrl.collectors import SyncDataCollector, aSyncDataCollector
 from torchrl.data import ReplayBuffer, LazyTensorStorage
 
-def make_collector(env: TransformedEnv, actor_model_explore: TensorDictSequential, config) -> SyncDataCollector:
+def make_collector(env: TransformedEnv, actor_model_explore: TensorDictSequential, config):
     collector = aSyncDataCollector(
         env,
         actor_model_explore,
         frames_per_batch=config.frames_per_batch,
         total_frames=config.total_frames,
-        update_at_each_batch=True,
     )
     collector.set_seed(config.seed)
     return collector
