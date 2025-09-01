595199be6f250817b45806ed8bc98957f8163197
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-09-01 19:34:14 +0700

    Перешел от симуляции ПИД к моделям из Gymnasium

--- scripts/train_simulation.py
+++ scripts/train_simulation.py
@@ -7,7 +7,7 @@ from torch.utils.tensorboard import SummaryWriter
 import hydra
 from omegaconf import DictConfig
 
-from nn_laser_stabilizer.train_utils import (
+from nn_laser_stabilizer.utils import (
     set_seeds
 )
 from nn_laser_stabilizer.agents.td3 import (
@@ -19,36 +19,27 @@ from nn_laser_stabilizer.agents.td3 import (
     train_step,
     warmup
 )
-from nn_laser_stabilizer.envs.utils import make_simulated_env, add_logger_to_env
+from nn_laser_stabilizer.envs.utils import make_simulated_env, make_gym_env
 
 from nn_laser_stabilizer.data.utils import make_buffer, make_sync_collector
-from nn_laser_stabilizer.config.find_configs_dir import find_configs_dir, DEFAULE_CONFIG_NAME
+from nn_laser_stabilizer.config.find_configs_dir import find_configs_dir
 
 from logging import getLogger
 logger = getLogger(__name__)
 
 from hydra.core.hydra_config import HydraConfig
 
-@hydra.main(config_path=find_configs_dir(), config_name=DEFAULE_CONFIG_NAME, version_base=None)
+@hydra.main(config_path=find_configs_dir(), config_name="train_simulation", version_base=None)
 def main(config: DictConfig) -> None:
     set_seeds(config.seed)
 
-    hydra_output_dir = HydraConfig.get().runtime.output_dir
-    env_log_dir = os.path.join(hydra_output_dir, "env_logs")
-    os.makedirs(env_log_dir, exist_ok=True)
-
-    env = make_simulated_env(config)
-    env = add_logger_to_env(env, env_log_dir)
-
-    train_log_dir = os.path.join(hydra_output_dir, "train_logs")
-    os.makedirs(train_log_dir, exist_ok=True)
-    train_writer = SummaryWriter(log_dir=train_log_dir)
+    env = make_gym_env(config)
 
     action_spec = env.action_spec_unbatched
     observation_spec = env.observation_spec_unbatched["observation"]
 
     actor, qvalue = make_td3_agent(config, observation_spec, action_spec)
-    actor_with_exploration, exploration_module = add_exploration(config, actor, action_spec)
+    actor_with_exploration, _ = add_exploration(config, actor, action_spec)
 
     warmup(env, actor_with_exploration, qvalue)
 
@@ -72,32 +63,43 @@ def main(config: DictConfig) -> None:
 
         for tensordict_data in collector:
             total_collected_frames += tensordict_data.numel()
-            buffer.extend(tensordict_data.unsqueeze(0).to_tensordict())
+            buffer.extend(tensordict_data)
+
+            rewards = tensordict_data.get(("next", "reward"))
+            steps = tensordict_data.get(("next", "step_count"))
+
+            avg_reward = rewards.mean().item()
+            logger.info(f"[Environment] Average reward per step = {avg_reward:.4f} at frame {total_collected_frames}")
+
+            max_episode_length = steps.max().item()
+            logger.info(f"[Environment] Max episode length = {max_episode_length} at frame {total_collected_frames}")
 
             # Exploration не используется здесь, поскольку он не используется в async версии
             # exploration_module.step(tensordict_data.numel())
 
-            if total_train_steps < train_config.total_train_steps:
-                for i in range(train_config.update_to_data):
-                    batch = buffer.sample(train_config.batch_size)
-                    
-                    update_actor = i % train_config.update_actor_freq == 0
-                    loss_qvalue_val, loss_actor_val = train_step(
-                        batch, loss_module, optimizer_actor, optimizer_critic, 
-                        target_net_updater, update_actor
-                    )
-                    
-                    recent_qvalue_losses.append(loss_qvalue_val)
-                    if loss_actor_val is not None:
-                        recent_actor_losses.append(loss_actor_val)
+            if total_train_steps >= train_config.total_train_steps:
+                break
+
+            for i in range(train_config.update_to_data):
+                batch = buffer.sample(train_config.batch_size)
                 
-                avg_qvalue_loss = sum(recent_qvalue_losses) / len(recent_qvalue_losses)
-                train_writer.add_scalar("Loss/Critic", avg_qvalue_loss, total_train_steps)
+                update_actor = i % train_config.update_actor_freq == 0
+                loss_qvalue_val, loss_actor_val = train_step(
+                    batch, loss_module, optimizer_actor, optimizer_critic, 
+                    target_net_updater, update_actor
+                )
+                
+                recent_qvalue_losses.append(loss_qvalue_val)
+                if loss_actor_val is not None:
+                    recent_actor_losses.append(loss_actor_val)
+            
+            avg_qvalue_loss = sum(recent_qvalue_losses) / len(recent_qvalue_losses)
+            logger.info(f"Loss/Q-function = {avg_qvalue_loss} on step {total_train_steps}")
 
-                avg_actor_loss = sum(recent_actor_losses) / len(recent_actor_losses)
-                train_writer.add_scalar("Loss/Actor", avg_actor_loss, total_train_steps)
+            avg_actor_loss = sum(recent_actor_losses) / len(recent_actor_losses)
+            logger.info(f"Loss/Actor = {avg_actor_loss} on step {total_train_steps}")
 
-                total_train_steps += 1
+            total_train_steps += 1
 
     except KeyboardInterrupt:
         logger.warning("Training interrupted by user.")
@@ -110,7 +112,6 @@ def main(config: DictConfig) -> None:
 
         collector.shutdown()
         env.close()
-        train_writer.close()
 
 if __name__ == "__main__":
     main()
