5998a77dd7635dda8a23f889d98050231a0aab02
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-09-13 15:38:49 +0700

    Вынес логеров в отдельный модуль

--- src/nn_laser_stabilizer/envs/utils.py
+++ src/nn_laser_stabilizer/envs/utils.py
@@ -1,5 +1,6 @@
 from torchrl.envs import TransformedEnv, DoubleToFloat, Compose, InitTracker
 
+from nn_laser_stabilizer.envs.logger import PerStepLoggerAsync
 from nn_laser_stabilizer.envs.pid_controller import PIDController
 from nn_laser_stabilizer.envs.oscillator import DuffingOscillator
 from nn_laser_stabilizer.envs.numerical_experimental_setup import NumericalExperimentalSetup
@@ -17,129 +18,6 @@ from torchrl.data import UnboundedContinuous, BoundedContinuous
 from tensordict import TensorDictBase
 import torch
 
-import threading
-import queue
-from pathlib import Path
-
-class SimpleFileLogger:
-    """
-    Простой логгер, который записывает все в один файл одной строкой с переносом.
-    """
-    
-    def __init__(self, log_dir: str): 
-        self.log_dir = Path(log_dir)
-        self.log_dir.mkdir(parents=True, exist_ok=True)
-        
-        self.log_file = self.log_dir / "log.txt"
-        self._file_handle = None
-        
-        print(f"SimpleFileLogger initialized. Logs: {self.log_file}")
-    
-    def log(self, message: str):
-        """
-        Записывает сообщение в файл.
-        """
-        # Предполгается однопоточная запись
-        if self._file_handle is None:
-            self._file_handle = open(self.log_file, 'w', encoding='utf-8')
-        
-        self._file_handle.write(message + "\n")
-        self._file_handle.flush()  # Сразу записываем на диск
-    
-    def close(self):
-        if self._file_handle:
-            self._file_handle.close()
-            self._file_handle = None
-    
-    def __del__(self):
-        try:
-            self.close()
-        except:
-            pass
-
-class PerStepLogger(Transform):
-    """
-    Логирует kp, ki, kd (из action) и x, setpoint (из observation) каждый шаг.
-    """
-    def __init__(self, log_dir: str = None):
-        super().__init__()
-        self.logger = SimpleFileLogger(log_dir=log_dir)
-        self._t = 0 
-
-    def _log_step(self, action_row: torch.Tensor, observation_row: torch.Tensor):
-        kp, ki, kd = action_row.tolist()
-        x, control_output, setpoint = observation_row.tolist()
-
-        log_line = f"step={self._t} kp={kp:.8f} ki={ki:.8f} kd={kd:.8f} x={x:.8f} control_output={control_output:.8f} setpoint={setpoint:.8f}"
-        self.logger.log(log_line)
-
-        self._t += 1
-
-    def _step(self, tensordict, next_tensordict):
-        action = tensordict.get("action", None)
-        observation = tensordict.get("observation", None)
-
-        if action is not None and observation is not None:
-            self._log_step(action, observation)
-
-        return next_tensordict
-    
-    def close(self):
-        self.logger.close()
-    
-    def __del__(self):
-        self.close()
-
-
-class PerStepLoggerAsync(Transform):
-    """
-    Логирует kp, ki, kd (из action) и x, setpoint (из observation) каждый шаг 
-    с использованием отдельного потока записи.
-    """
-    def __init__(self, log_dir: str = None):
-        super().__init__()
-        self.logger = SimpleFileLogger(log_dir=log_dir)
-        self._t = 0
-        self._q = queue.Queue(maxsize=100_000)
-        self._stop = False
-
-        self._thread = threading.Thread(target=self._worker, daemon=True)
-        self._thread.start()
-
-    def _log_step_async(self, action_row, observation_row):
-        try:
-            kp, ki, kd = action_row.tolist()
-            x, control_output, setpoint = observation_row.tolist()
-            
-            log_line = f"step={self._t} kp={kp:.8f} ki={ki:.8f} kd={kd:.8f} x={x:.8f} control_output={control_output:.8f} setpoint={setpoint:.8f}"
-            self._q.put_nowait(log_line)
-        except queue.Full:
-            pass
-        finally:
-            self._t += 1
-
-    def _worker(self):
-        while not self._stop:
-            try:
-                log_line = self._q.get(timeout=0.1)
-                self.logger.log(log_line)
-            except queue.Empty:
-                continue
-
-    def _step(self, tensordict, next_tensordict):
-        action = tensordict.get("action", None)
-        observation = tensordict.get("observation", None)
-        if action is not None and observation is not None:
-            self._log_step_async(action, observation)
-        return next_tensordict
-    
-    def close(self):
-        self._stop = True
-        self._thread.join()
-        self.logger.close()
-    
-    def __del__(self):
-        self.close()
 
 class ObservationActionConcat(Transform):
     def __init__(self, obs_key="observation", action_key="action",
