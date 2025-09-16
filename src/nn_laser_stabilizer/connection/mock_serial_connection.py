a5c9e74cefd869d6ab42d747c21f910d4dc3d363
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-09-16 20:48:09 +0700

    Добавил запись числа шагов в MockSerialConnection

--- src/nn_laser_stabilizer/connection/mock_serial_connection.py
+++ src/nn_laser_stabilizer/connection/mock_serial_connection.py
@@ -1,16 +1,17 @@
 import time
+import random
 from typing import Optional
+
+from nn_laser_stabilizer.envs.constants import ADC_MAX, DAC_MAX
 
 class MockSerialConnection:
     def __init__(self, port: str, timeout: float = 0.1, baudrate: int = 115200):
         self.port = port
         self.timeout = timeout
         self.baudrate = baudrate
-
         self.is_connected = False
 
-        self.process_variable = 1.0
-        self.control_output = 1.0
+        self._step = 0
 
     def open_connection(self):
         self.is_connected = True
@@ -24,13 +25,16 @@ class MockSerialConnection:
         if not self.is_connected:
             raise ConnectionError("[MOCK_SERIAL_CONNECTION] Serial connection is not open.")
         
-        response = f"{int(self.process_variable)} {int(self.control_output)}"
-        print(f"[MOCK_SERIAL_CONNECTION] Read: '{response}'")
+        process_variable = random.randint(0, ADC_MAX)
+        control_output = random.randint(0, DAC_MAX)
+        response = f"{process_variable} {control_output}"
+        self._step += 1
+
+        print(f"[MOCK_SERIAL_CONNECTION] Read: '{response}' Step: '{self._step}'")
         time.sleep(max(0.001, self.timeout * 0.1))
         return response
 
     def send_data(self, data_to_send: str):
         if not self.is_connected:
             raise ConnectionError("[MOCK_SERIAL_CONNECTION] Serial connection is not open.")
-        
-        print(f"[MOCK_SERIAL_CONNECTION] Send: '{repr(data_to_send)}'")
+        print(f"[MOCK_SERIAL_CONNECTION] Send: '{data_to_send}' Step: '{self._step}'")
