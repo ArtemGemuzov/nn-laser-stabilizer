4d04684d90867b044587029e0d0c93a48da3fd94
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-09-25 11:50:52 +0700

    Перенес логера в отдельную директорию

--- src/nn_laser_stabilizer/envs/logger.py
+++ src/nn_laser_stabilizer/envs/logger.py
@@ -1,46 +1,10 @@
-from pathlib import Path
 from queue import Queue, Full, Empty
 from threading import Thread
 import time
 
 from torchrl.envs import EnvBase, Transform
 
-
-class SimpleFileLogger:
-    """
-    Простой логгер, который записывает все в один файл одной строкой с переносом.
-    """
-
-    def __init__(self, log_dir: str):
-        self.log_dir = Path(log_dir)
-        self.log_dir.mkdir(parents=True, exist_ok=True)
-
-        self.log_file = self.log_dir / "log.txt"
-        self._file_handle = None
-
-        print(f"SimpleFileLogger initialized. Logs: {self.log_file}")
-
-    def log(self, message: str):
-        """
-        Записывает сообщение в файл.
-        """
-        # Предполгается однопоточная запись
-        if self._file_handle is None:
-            self._file_handle = open(self.log_file, 'w', encoding='utf-8')
-
-        self._file_handle.write(message + "\n")
-        self._file_handle.flush()  # Сразу записываем на диск
-
-    def close(self):
-        if self._file_handle:
-            self._file_handle.close()
-            self._file_handle = None
-
-    def __del__(self):
-        try:
-            self.close()
-        except:
-            pass
+from nn_laser_stabilizer.logging.file_logger import SimpleFileLogger
 
 
 class PerStepLoggerAsync(Transform):
