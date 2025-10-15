567f12bde43b1112636d4ddc73a6104ba70f27f2
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-10-15 11:10:23 +0700

    Перенес make методы для коллекторов и буфера в модуль training

---
+++ src/nn_laser_stabilizer/training/__init__.py
@@ -0,0 +1,11 @@
+from nn_laser_stabilizer.training.data_utils import (
+    make_buffer, 
+    make_sync_collector, 
+    make_async_collector,
+)
+
+__all__ = [
+    'make_buffer', 
+    'make_sync_collector', 
+    'make_async_collector',
+]
