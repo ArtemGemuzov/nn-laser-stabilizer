896c572789729469a605694074004b76f36f77ec
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-11-18 18:26:46 +0700

    Добавил базовый класс для критика

---
+++ src/nn_laser_stabilizer/critic.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+import torch
+import torch.nn as nn
+
+
+class Critic(nn.Module, ABC):
+    def __init__(self, obs_dim: int, action_dim: int):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.action_dim = action_dim
+    
+    @abstractmethod
+    def forward(self, observation: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @torch.no_grad()
+    def eval(self, observation: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
+        return self(observation, action)
