ecf055647649f68341727f485c04c2659b8c0081
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2025-11-25 10:24:38 +0700

    Добавил логирование времени в скрипты обучения

--- scripts/train_sync.py
+++ scripts/train_sync.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import time
 
 import numpy as np
 
@@ -139,18 +140,19 @@ def main(context: ExperimentContext):
             )
             
             if step % log_frequency == 0:
+                timestamp = time.time()
                 if actor_loss is not None:
-                    train_logger.log(f"step={step} loss_q1={loss_q1:.4f} loss_q2={loss_q2:.4f} actor_loss={actor_loss:.4f} buffer_size={len(buffer)}")
+                    train_logger.log(f"step={step} time={timestamp:.6f} loss_q1={loss_q1:.4f} loss_q2={loss_q2:.4f} actor_loss={actor_loss:.4f} buffer_size={len(buffer)}")
                 else:
-                    train_logger.log(f"step={step} loss_q1={loss_q1:.4f} loss_q2={loss_q2:.4f} buffer_size={len(buffer)}")
+                    train_logger.log(f"step={step} time={timestamp:.6f} loss_q1={loss_q1:.4f} loss_q2={loss_q2:.4f} buffer_size={len(buffer)}")
             
             if step % validation_frequency == 0 and step > 0:
                 rewards = validate(actor, lambda: make_env_from_config(env_config), num_steps=validation_num_steps)
-                train_logger.log(f"validation step={step} reward_sum={rewards.sum():.4f} reward_mean={rewards.mean():.4f} episodes={rewards.size}")
+                train_logger.log(f"validation step={step} time={time.time():.6f} reward_sum={rewards.sum():.4f} reward_mean={rewards.mean():.4f} episodes={rewards.size}")
         
         context.console_logger.log("Final validation...")
         final_rewards = validate(actor, lambda: make_env_from_config(context.config.env), num_steps=final_validation_num_steps)
-        train_logger.log(f"final_validation reward_sum={final_rewards.sum():.4f} reward_mean={final_rewards.mean():.4f} episodes={final_rewards.size}")
+        train_logger.log(f"final_validation time={time.time():.6f} reward_sum={final_rewards.sum():.4f} reward_mean={final_rewards.mean():.4f} episodes={final_rewards.size}")
         
         context.console_logger.log("Saving models...")
         models_dir = context.models_dir
