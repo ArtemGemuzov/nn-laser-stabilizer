582c11edae7f3547f7d0979f95e9af6fad0e6e27
Artem Gemuzov <a.gemuzov@g.nsu.ru>
2026-02-09 13:48:37 +0700

    Вынес вычисление приращения в отдельный класс. Добавил предвычисление коэффициентов

--- src/nn_laser_stabilizer/pid.py
+++ src/nn_laser_stabilizer/pid.py
@@ -23,15 +23,33 @@ class PID:
         min_output: float,
         max_output: float,
     ) -> None:
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.dt = dt
+        self._kp = kp
+        self._ki = ki
+        self._kd = kd
+        self._dt = dt
+        self._ki_dt = ki * dt
+        self._kd_over_dt = kd / dt
         self.min_output = min_output
         self.max_output = max_output
 
         self._integral: float = 0.0
         self._prev_error: float = 0.0
+
+    @property
+    def kp(self) -> float:
+        return self._kp
+
+    @property
+    def ki(self) -> float:
+        return self._ki
+
+    @property
+    def kd(self) -> float:
+        return self._kd
+
+    @property
+    def dt(self) -> float:
+        return self._dt
 
     @classmethod
     def from_config(cls, config: PIDConfig) -> "PID":
@@ -48,19 +66,95 @@ class PID:
         error = process_variable - setpoint
 
         self._integral = np.clip(
-            self._integral + error * self.dt * self.ki,
+            self._integral + error * self._ki_dt,
             self.min_output,
             self.max_output,
         )
 
-        derivative = (error - self._prev_error) / self.dt
+        derivative = (error - self._prev_error) * self._kd_over_dt
         self._prev_error = error
 
-        output = error * self.kp + self._integral + derivative * self.kd
+        output = error * self._kp + self._integral + derivative
         return np.clip(output, self.min_output, self.max_output)
 
     def __call__(self, process_variable: float, setpoint: float) -> float:
         return self.compute(process_variable, setpoint)
+
+
+class PIDDelta:
+    """Вычисляет приращение (дельту) инкрементального ПИД-регулятора.
+
+    Предвычисленные коэффициенты позволяют свести вычисление к трём умножениям:
+        delta = c0 * error + c1 * prev_error + c2 * prev_prev_error
+    где:
+        c0 = kp + ki * dt + kd / dt
+        c1 = -(kp + 2 * kd / dt)
+        c2 = kd / dt
+    """
+
+    def __init__(
+        self,
+        kp: float,
+        ki: float,
+        kd: float,
+        dt: float,
+    ) -> None:
+        self._kp = kp
+        self._ki = ki
+        self._kd = kd
+        self._dt = dt
+        
+        self._c0 = kp + ki * dt + kd / dt
+        self._c1 = -(kp + 2.0 * kd / dt)
+        self._c2 = kd / dt
+
+        self._prev_error: float = 0.0
+        self._prev_prev_error: float = 0.0
+
+    @property
+    def kp(self) -> float:
+        return self._kp
+
+    @property
+    def ki(self) -> float:
+        return self._ki
+
+    @property
+    def kd(self) -> float:
+        return self._kd
+
+    @property
+    def dt(self) -> float:
+        return self._dt
+
+    @classmethod
+    def from_config(cls, config: PIDConfig) -> "PIDDelta":
+        return cls(
+            kp=config.kp,
+            ki=config.ki,
+            kd=config.kd,
+            dt=config.dt,
+        )
+
+    def compute(self, process_variable: float, setpoint: float) -> float:
+        error = process_variable - setpoint
+
+        delta = (
+            self._c0 * error
+            + self._c1 * self._prev_error
+            + self._c2 * self._prev_prev_error
+        )
+
+        self._prev_prev_error = self._prev_error
+        self._prev_error = error
+        return delta
+
+    def __call__(self, process_variable: float, setpoint: float) -> float:
+        return self.compute(process_variable, setpoint)
+
+    def reset(self) -> None:
+        self._prev_error = 0.0
+        self._prev_prev_error = 0.0
 
 
 class IncrementalPID:
@@ -73,16 +167,27 @@ class IncrementalPID:
         min_output: float,
         max_output: float,
     ) -> None:
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.dt = dt
+        self._delta = PIDDelta(kp=kp, ki=ki, kd=kd, dt=dt)
         self.min_output = min_output
         self.max_output = max_output
 
-        self._prev_error: float = 0.0
-        self._prev_prev_error: float = 0.0
         self._output: float = 0.0
+
+    @property
+    def kp(self) -> float:
+        return self._delta.kp
+
+    @property
+    def ki(self) -> float:
+        return self._delta.ki
+
+    @property
+    def kd(self) -> float:
+        return self._delta.kd
+
+    @property
+    def dt(self) -> float:
+        return self._delta.dt
 
     @classmethod
     def from_config(cls, config: PIDConfig) -> "IncrementalPID":
@@ -96,22 +201,14 @@ class IncrementalPID:
         )
 
     def compute(self, process_variable: float, setpoint: float) -> float:
-        error = process_variable - setpoint
-
-        delta_p = self.kp * (error - self._prev_error)
-        delta_i = self.ki * error * self.dt
-        delta_d = self.kd * (error - 2 * self._prev_error + self._prev_prev_error) / self.dt
-
-        delta_output = delta_p + delta_i + delta_d
+        delta = self._delta.compute(process_variable, setpoint)
 
         self._output = np.clip(
-            self._output + delta_output,
+            self._output + delta,
             self.min_output,
             self.max_output,
         )
 
-        self._prev_prev_error = self._prev_error
-        self._prev_error = error
         return self._output
 
     def __call__(self, process_variable: float, setpoint: float) -> float:
